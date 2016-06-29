3b5d6fd57977abfb4aed18dcce5d3e34e170760d
Michael Marion <michaelmarion@icloud.com>
2016-06-28 22:29:02 -0400

    Mobile styling.

--- public/javascripts/navigation.js
+++ public/javascripts/navigation.js
@@ -1,8 +1,8 @@
-console.log('loaded');
-
 $(document).on('ready', function() {
   $('#nameplate').click(function() {
     $('#navigation').toggleClass('showing');
+    $('#overlay').toggleClass('showing');
     $('#nameplate h1').toggleClass('menu');
+    $('#content').toggleClass('prevent-scroll');
   });
 });
