84d4f098859595b62248e999e619907dda2bdb70
Michael Marion <michaelmarion@icloud.com>
2017-06-25 19:55:36 -0400

    Modularize app.js.

--- app.js
+++ app.js
@@ -1,51 +1,12 @@
-// Application
 var express = require('express');
 var app = express();
 
-// Logging
-var logger = require('morgan');
-app.use(logger('dev'));
-
-// View Engiine
-var path = require('path');
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// Body Parsing
-var body = require('body-parser');
-app.use(body.json());
-app.use(body.urlencoded({ extended: false }));
-
-// Cookie Parsing
-var cookie = require('cookie-parser');
-app.use(cookie());
-app.use(express.static(path.join(__dirname, 'public')));
-
-//Routes
+require('./config/static')(express, app);
+require('./config/views')(app);
+require('./config/logger')(app);
+require('./config/parsers')(app);
+require('./config/sass')(app);
 require('./routes/router')(app);
-
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
+require('./config/error')(app);
 
 module.exports = app;
