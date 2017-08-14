bb23b3e831a7eb2765f793edf603e7379049c19a
Michael Marion <michaelmarion@icloud.com>
2017-08-14 19:23:26 -0400

    Add SASS middleware.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 
 require('./config/static')(express, app);
+require('./config/sass')(express, app);
 require('./config/views')(app);
 require('./config/logger')(app);
 require('./config/parsers')(app);
---
+++ config/sass.js
@@ -0,0 +1,16 @@
+var sass = require('node-sass-middleware');
+var path = require('path');
+
+module.exports = function(express, app) {
+  app.use(sass({
+      /* Options */
+      src: __dirname,
+      dest: path.join(__dirname, 'public'),
+      debug: true,
+      outputStyle: 'compressed',
+      prefix:  '/prefix'  // Where prefix is at <link rel="stylesheets" href="prefix/style.css"/>
+  }));
+  // Note: you must place sass-middleware *before* `express.static` or else it will
+  // not work.
+  app.use('/public', express.static(path.join(__dirname, 'public')));
+}
